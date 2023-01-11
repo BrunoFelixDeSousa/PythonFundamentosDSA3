cea3e0c84de107be41ed5225c42eff2c0b47113a
brunofelixdesousa <brunofelixdesousa@hotmail.com>
2023-01-11 00:17:37 -0300

    cap5

--- Cap03/Lab02/calculadora_v1.py
+++ Cap03/Lab02/calculadora_v1.py
@@ -6,4 +6,57 @@
 
 print("\n******************* Python Calculator *******************")
 
+def add(x, y):
+	return x + y
 
+def subtract(x, y):
+	return x - y
+
+def multiply(x, y):
+	return x * y
+
+def divide(x, y):
+	return x / y
+
+def menu():
+    print("\nSelecione o número da operação desejada: \n")
+    print("1 - Soma")
+    print("2 - Subtração")
+    print("3 - Multiplicação")
+    print("4 - Divisão")
+    print("5 - Fechar Programa")
+
+def choice():
+    escolha = input("\nDigite sua opção (1/2/3/4/5): ")
+
+    while escolha != '5':
+        num1 = int(input("\nDigite o primeiro número: "))
+        num2 = int(input("\nDigite o segundo número: "))
+
+        if escolha == '1':
+            print("\n")
+            print(num1, "+", num2, "=", add(num1, num2))
+            print("\n")
+
+        elif escolha == '2':
+            print("\n")
+            print(num1, "-", num2, "=", subtract(num1, num2))
+            print("\n")
+
+        elif escolha == '3':
+            print("\n")
+            print(num1, "*", num2, "=", multiply(num1, num2))
+            print("\n")
+
+        elif escolha == '4':
+            print("\n")
+            print(num1, "/", num2, "=", divide(num1, num2))
+            print("\n")
+
+        else:
+            print("\nOpção Inválida!")
+
+
+if __name__ == '__main__':
+    menu()
+    choice()
